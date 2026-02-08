503cfde2299f2e9852a8d2acf91164329608fe31
javierlibre <javierlibre13@gmail.com>
2026-02-07 22:58:27 -0500

    Webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,10 +20,6 @@ class Log(db.Model):
 with app.app_context():
   db.create_all()
 
-  prueba1 = Log(texto="Prueba 1")
-  db.session.add(prueba1)
-  db.session.commit()
-
 #función para ordenar los mensajes por fecha y hora
 def ordenar_por_fecha_y_hora(registros):
   return sorted(registros, key=lambda x: x.fecha_y_hora, reverse=True)
@@ -38,13 +34,40 @@ def index():
 mensajes_log = []
 
 #función para agregar mensajes y guardarlos en la base de datos
-def agregar_mensaje_log(texto):
+def agregar_mensajes_log(texto):
   mensajes_log.append(texto)
 
   #guardar el mensaje en la base de datos
   nuevo_registro = Log(texto=texto)
   db.session.add(nuevo_registro)
   db.session.commit()
+
+
+#Token de verificación para la configuración del webhook
+TOKEN_ANDERCODE = 'ANDERCODE'
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+  if request.method == 'GET':
+    challenge = verificar_token(request)
+    return challenge
+  elif request.method == 'POST':
+    response = recibir_mensajes(request)
+    return response
+  
+def verificar_token(req):
+  token = req.args.get('hub.verify_token')
+  challenge = req.args.get('hub.challenge')
+  if challenge and token == TOKEN_ANDERCODE:
+    return challenge
+  else:
+    return jsonify({'error': 'Token de verificación incorrecto'}), 401
+  
+
+def recibir_mensajes(req):
+  req = request.get_json()
+  agregar_mensajes_log(req)
+  return jsonify({'message': 'EVENT_RECEIVED'})
 
 if __name__ == '__main__':
   #app.run(debug=True)

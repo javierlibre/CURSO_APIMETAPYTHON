6f990a3e168420889cac7fdf02172890187e4b25
javierlibre <javierlibre13@gmail.com>
2026-02-08 12:32:05 -0500

    Webhook 2

--- app.py
+++ app.py
@@ -68,9 +68,19 @@ def verificar_token(req):
   
 
 def recibir_mensajes(req):
-  req = request.get_json()
-  agregar_mensajes_log(req)
-  return jsonify({'message': 'EVENT_RECEIVED'})
+   try:
+        req = request.get_json()
+        entry =req['entry'][0]
+        changes = entry['changes'][0]
+        value = changes['value']
+        objeto_mensaje = value['messages']
+
+        agregar_mensajes_log(objeto_mensaje)
+
+        return jsonify({'message': 'EVENT_RECEIVED'})
+   
+   except Exception as e:
+        return jsonify({'message': 'EVENT_RECEIVED'})
 
 if __name__ == '__main__':
   #app.run(debug=True)

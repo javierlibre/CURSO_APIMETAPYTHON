2927f3d35d8bacb8b7722e6775d90a274c3b3b02
javierlibre <javierlibre13@gmail.com>
2026-03-06 01:58:07 -0500

    lista academy 22

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def enviar_mensajes_whatsapp(texto, number):
                             "title":"Nuestros Servicios",
                             "rows":[
                                 {
-                                    "id":"btnsigns_eng",
+                                    "id":"btnsigns_spa",
                                     "title" : "Signs and Signages",
                                     "description": "We offer a variety of signs and signages for your business."
                                 },
